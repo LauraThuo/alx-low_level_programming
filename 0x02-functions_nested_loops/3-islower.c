72476860cc63a6277afea0df61a42ffef71e7727
LauraThuo <laurawthuo@gmail.com>
2023-02-26 22:19:33 -0800

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,28 +2,6 @@
 
 
 	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    print_alphabet_x10();
-	    return (0);
-	}
-
-
-
-=====================================
-
-
-3-islower.c CODE
-
-
-#include "main.h"
-
-
-	/**
 	 * _islower - check if char is lowercase
 	 * @c: is the char to be checked
 	 * Return: 1 if char is lowercase, otherwise 0.
